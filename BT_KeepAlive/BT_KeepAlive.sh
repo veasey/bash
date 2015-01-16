f423abdc2ea640ea257e6e3c1fe1a1cec054497f
Sean Todd <gigabyte4711@googlemail.com>
2015-01-16 02:52:10 +0000

    Better error and Network handling.
    Added checks for disconnected access points by ensuring you're connected to a BTOpenzone AP before even trying any pings. Fixed issue where script was creating multiple versions of the same connection in network manager by re-using an existing profile if one exists. If not, it will create one. Added code to connect via a specific AP, if several identical SSID's are available. Currently, the script forces your conection to the nearest BTOpenzone. This is useful if scripted for autoconnection, but may cause issues if not terminated before connecting to another network.

--- BT_KeepAlive/BT_KeepAlive.sh
+++ BT_KeepAlive/BT_KeepAlive.sh
@@ -10,14 +10,25 @@ essid=BTOpenzone
 attempts=1
 
 ConnectifyMe() {
-  # 3 Pings, push POST request if 100% loss of connectivity.	
-  if ping -c 3 $internet | grep '100% packet loss\|Network is unreachable' ; then    
-	echo "$(date "+%Y-%m-%d %H:%M:%S:") Connection down"
-    if iwconfig | grep "BTOpenzone" ; then 
-	  curl 'https://www.btopenzone.com:8443/ante' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-gb,en;q=0.5' -H 'Connection: keep-alive' -H 'Cookie: JSESSIONID=716ri2hfsar64; __utma=171794931.404001753.1385254451.1385254451.1385254451.1; __utmb=171794931.3.10.1385254451; __utmc=171794931; __utmz=171794931.1385254451.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); s_cc=true; s_sq=%5B%5BB%5D%5D' -H 'Host: www.btopenzone.com:8443' -H 'Referer: https://www.btopenzone.com:8443/wpb' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0' -H 'Content-Type: application/x-www-form-urlencoded' --data "username=$username&password=$password&x=0&y=0&xhtmlLogon=https%3A%2F%2Fwww.btopenzone.com%3A8443%2Fante" > /dev/null
-    else    
-   		echo "Restarting Network Service"
+#Checks to make sure you are actually connected to a BT Openzone Access Point.
+    if iwgetid -r | grep "BTOpenzone" 2>/dev/null >/dev/null ; then  
+ # 3 Pings, push POST request if 100% loss of connectivity.	
+if ping -c 3 $internet 2>/dev/null >/dev/null | grep '100% packet loss\|Network is unreachable' ; then 
+echo "$(date "+%Y-%m-%d %H:%M:%S:") Connection down"
+ SendLoginPOST
+else
+    echo "$(date "+%Y-%m-%d %H:%M:%S:") Online"
+    sleep 3
+  fi
 
+else
+ConnectToAP
+fi
+  
+}
+
+ConnectToAP(){
+   		
 		# The following code I might get rid of. It is useful if you
 		# have more than one wireless NIC, as this next part resolves
 		# a hardware conflict.
@@ -28,16 +39,28 @@ ConnectifyMe() {
 		# nmcli nm wifi off
 		# nmcli nm wifi on
 		# Disconnect onboard WLAN if my USB is plugged in.
-        # nmcli dev disconnect iface wlan0
+       		# nmcli dev disconnect iface wlan0
 
-		echo "Connecting to Access Point"
-        #nmcli dev wifi connect $essid
-		nmcli dev wifi connect $essid iface wlan1
-	fi
-  else
-    echo "$(date "+%Y-%m-%d %H:%M:%S:") Online"
-    sleep 3
-  fi
+		echo "Connecting to BT Openzone Access Point"
+		
+		#This connects to any BT Openzone Access Point
+		
+		#Check to see if BTOpenzone is already a saved connection
+		if nmcli c | grep $essid 2>/dev/null >/dev/null;
+		#If so, connect to it
+then nmcli c up id $essid iface wlan1 2>/dev/null >/dev/null
+else 		
+		#If not, create a new connection for it.
+nmcli dev wifi connect $essid iface wlan1 2>/dev/null >/dev/null
+		#This connects to a specific Access Point using BSSID (Use this if you know What you're doing. eg. A specific AP is faster than all others.)
+		#nmcli dev wifi connect 00:3A:99:17:A5:80 iface wlan1 2>/dev/null >/dev/null
+fi
+		
+		
+}
+
+SendLoginPOST(){
+curl -k 'https://www.btopenzone.com:8443/ante' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-gb,en;q=0.5' -H 'Connection: keep-alive' -H 'Cookie: JSESSIONID=716ri2hfsar64; __utma=171794931.404001753.1385254451.1385254451.1385254451.1; __utmb=171794931.3.10.1385254451; __utmc=171794931; __utmz=171794931.1385254451.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); s_cc=true; s_sq=%5B%5BB%5D%5D' -H 'Host: www.btopenzone.com:8443' -H 'Referer: https://www.btopenzone.com:8443/wpb' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0' -H 'Content-Type: application/x-www-form-urlencoded' --data "username=$username&password=$password&x=0&y=0&xhtmlLogon=https%3A%2F%2Fwww.btopenzone.com%3A8443%2Fante" 2>/dev/null >/dev/null
 }
 
 DecryptCheck() {
